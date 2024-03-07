6a4e033e5bf87fe5b4ae90efdcf6a8b4bdd9d7bd
LAlinaV <144618212+LAlinaV@users.noreply.github.com>
2024-03-07 07:57:00 +0000

    create files, code for interface

---
+++ src/interface.h
@@ -0,0 +1,13 @@
+//
+// created by Alina Ladik
+//
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+//function findDegree is used in client file (main.c)
+//implementation is in server file (interface.h)
+
+bool findDegree(int n,  int arr[n]);
+
+#endif
